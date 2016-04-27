cf86da915817a9020351e28ee1da9ad3a7b8b4f0
raimundopcjr <raimundopcjr@gmail.com>
2016-04-27 05:13:34 -0300

    get slected User current Node

---
+++ app/src/main/java/br/com/ngccodex/yourtasks/ui/UserEditActivity.java
@@ -0,0 +1,40 @@
+package br.com.ngccodex.yourtasks.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import br.com.ngccodex.yourtasks.R;
+import br.com.ngccodex.yourtasks.storage.firebase.FirebaseBaseMain;
+
+public class UserEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_edit);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+        String curNode;
+        if (extras != null) {
+            curNode = extras.getString(FirebaseBaseMain.EXTRA_CURRENT_USER_NODE);
+        }
+
+    }
+
+}
